0dbab244891c03dd1e9bdd0f403f86d49dd365cd
heguohui2018 <heguohui2017@outlook.com>
2020-09-11 15:55:25 +0800

    head 标签内容

---
+++ include/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>CMS Project</title>
+
+        <!-- Bootstrap  核心css文件 -->
+        <link href="../css/bootstrap.min.css" rel="stylesheet" />
+
+        <!-- 样式文件 -->
+        <link href="../css/blog-home.css" rel="stylesheet" />
+        <link href="../css/styles.css" rel="stylesheet" />
+
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body></body>
+</html>
