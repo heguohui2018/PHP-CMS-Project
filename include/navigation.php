b4525982a5cb45d70995dfebc139f7e8c4de6e90
heguohui2018 <heguohui2017@outlook.com>
2020-09-12 01:34:43 +0800

    导航条：后续再修改

---
+++ include/navigation.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">LOGO</a>
+		</div>
+		<div>
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">导航1</a></li>
+				<li><a href="#">导航二</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						下拉菜单
+						<b class="caret"></b>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a href="#">下拉菜单1</a></li>
+						<li><a href="#"></a></li>
+						<li><a href="#">下拉菜单2</a></li>
+						<li class="divider"></li>
+						<li><a href="#">下拉菜单3</a></li>
+						<li class="divider"></li>
+						<li><a href="#">下拉菜单4</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		<!-- 导航右侧 -->
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="#"><span class="glyphicon glyphicon-user"></span> 注册</a></li>
+			<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> 登录</a></li>
+		</ul>
+	</div>
+</nav>
