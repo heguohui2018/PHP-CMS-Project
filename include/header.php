793dfda99f113826116dac82849a3bfec70c76a3
heguohui2018 <heguohui2017@outlook.com>
2020-09-12 01:35:50 +0800

    head 标签部分：使用国内cdn服务

--- include/header.php
+++ include/header.php
@@ -1,23 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>CMS Project</title>
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content="" />
+	<meta name="author" content="" />
+	<title>CMS Project</title>
 
-        <!-- Bootstrap  核心css文件 -->
-        <link href="../css/bootstrap.min.css" rel="stylesheet" />
+	<!-- Bootstrap  核心css文件 -->
+	<link href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
-        <!-- 样式文件 -->
-        <link href="../css/blog-home.css" rel="stylesheet" />
-        <link href="../css/styles.css" rel="stylesheet" />
+	<!-- 样式文件 -->
 
-        <!-- jQuery -->
-        <script src="js/jquery.js"></script>
-        <!-- Bootstrap Core JavaScript -->
-        <script src="js/bootstrap.min.js"></script>
-    </head>
-    <body></body>
-</html>
+	<!-- 脚本文件 -->
+
+	<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+	<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+	<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+
+	<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
