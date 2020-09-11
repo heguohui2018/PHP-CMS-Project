8ab428a9dd6c381dd629a746aa8e177bcc2f7036
heguohui2018 <heguohui2017@outlook.com>
2020-09-11 16:03:55 +0800

    数据库配置文件

---
+++ include/db.php
@@ -0,0 +1,8 @@
+<?php
+ob_start();
+$db_hsot = 'localhsot';
+$db_user = 'root';
+$db_password = 'heguohui2018';
+$db_name = 'cms';
+
+$connection = mysqli_connect($db_hsot, $db_user, $db_password, $db_hsot);
